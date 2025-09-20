be9542cbe2dc8b83e479e541cd82464e2fabd5f5
ZakariaRek <zakariaest49@gmail.com>
2025-09-20 16:22:06 +0100

    project structure

---
+++ AlertSignal/src/main/java/com/alert_signals/AlertSignal/AlertSignalApplication.java
@@ -0,0 +1,13 @@
+package com.alert_signals.AlertSignal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertSignalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertSignalApplication.class, args);
+	}
+
+}
---
+++ MarketImpact/src/main/java/com/market_impact/MarketImpact/MarketImpactApplication.java
@@ -0,0 +1,13 @@
+package com.market_impact.MarketImpact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketImpactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketImpactApplication.class, args);
+	}
+
+}
